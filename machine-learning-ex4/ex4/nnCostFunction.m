345f9db260fbba60763e554c095c38022d8033d3
Han Qiang <qiang.a.han@gmail.com>
2016-12-10 08:41:32 +0800

    machine learning ww05
    Signed-off-by: Han Qiang <qiang.a.han@gmail.com>

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,32 +62,11 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-%size(nn_params)
-%nn_params
-%input_layer_size
-%hidden_layer_size
-%num_labels
-%
-%size(X)
-%X
-%size(y)
-%y
-%
-%size(Theta1)
-%Theta1
-%size(Theta2)
-%Theta2
-%
-%lambda
-
-m = size(X, 1);
-K = num_labels;
-
 for i=1:size(y)(1)
-	test_y(i,:) = eye(K)(y(i),:);
+	test_y(i,:) = eye(num_labels)(y(i),:);
 end
 
-X = [ones(m, 1) X];
+% X = [ones(m, 1) X];
 
 reg = 0;
 for j=1:size(Theta1)(1)
@@ -107,7 +86,8 @@ end
 reg = lambda*reg/(2*m);
 
 for i=1:m
-	a2 = sigmoid(Theta1*X(i,:)');
+	a1 = [1; X(i,:)'];
+	a2 = sigmoid(Theta1*a1);
 	a2 = [1; a2];
 	a3 = sigmoid(Theta2*a2);
 	J += (-test_y(i,:)*log(a3) - (1 - test_y(i,:))*log(1-a3));
@@ -116,51 +96,29 @@ end
 
 J /= m;
 
-
-%J = sum(-y.*log(sigmoid(X*theta)) - (1 - y).*log(1-sigmoid(X*theta))/m;
-
-%grad = ((sigmoid(X*theta) - y)'*X)'/m + lambda*theta/m;
-%grad(1,1) = (((sigmoid(X*theta) - y)'*X)'/m)(1,1);
-
+D1 = 0;
+D2 = 0;
 for t=1:m
-	a1 = X(t,:);
-	z2 = Theta1*a1';
+	y = test_y(t,:)';
+	a1 = [1; X(t,:)'];
+	z2 = Theta1*a1;
 	a2 = [1; sigmoid(z2)];
 	z3 = Theta2*a2;
-	a3 = sigmoid(z3)
-	test_y(t,:)'
-	% delta3(:,t) = a3 - test_y(t,:)';
-	% delta2(:,t) = Theta2'*(delta3(:,t).*sigmoidGradient(z2));
-	delta3 = a3 - test_y(t,:)'
-	size(Theta2')
-	size(delta3)
-	size(z2)
-	size(sigmoidGradient(z2))
-	size(delta3.*sigmoidGradient(z2))
-	delta2 = Theta2'*(delta3.*sigmoidGradient(z2));
-	% delta2 = delta2(2:end)
-	% size(delta3(:,t))
-	% size(delta2(:,t))
-	% size(delta2)
-	% size(a2)
-	% [0; delta3(:,t)]'
-	% a2
-	% [0; delta3(:,t)]'*a2
-	% D2 += [0; delta3(:,t)]'*a2;
-	% D1 += delta2(:,t)'*a1'
-	delta2 = delta2(2:end);
-	if t == 1
-		D2 = delta3 * a2';
-		D1 = delta2 * a1;
-	else
-		D2 += delta3 * a2';
-		D1 += delta2 * a1;
-end
+	a3 = sigmoid(z3);
 
-Theta1_grad = D1/m;
-Theta2_grad = D2/m;
+	delta3 = a3 - y;
+	% there is no need to remove or skip anything from delta(2).
+	% must remove the first column of Theta2 when you perform this calculation
+	% https://www.coursera.org/learn/machine-learning/programming/AiHgN/neural-network-learning/discussions/threads/7SC2RK2iEeamngrNZbefFg
+	delta2 = (Theta2(:,2:end)' * delta3) .* sigmoidGradient(z2);
+	D2 = D2 + (delta3 * a2');
+	D1 = D1 + (delta2 * a1');
+endfor
 
-
+Theta1(:,1) = 0;
+Theta2(:,1) = 0;
+Theta1_grad = D1/m + lambda*Theta1/m;
+Theta2_grad = D2/m + lambda*Theta2/m;
 
 
 
